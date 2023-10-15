b4cb97076de82e4153466ade4cc1a74f4e481dd6
Sahej Singh <sahej3526@gmail.com>
2023-10-15 16:33:58 -0500

    finished testTicker

--- backtestingClass.py
+++ backtestingClass.py
@@ -1,24 +1,57 @@
-from typing import Callable, List, Dict
-import yfinance as yf
-from pandas import DataFrame
-
+from typing import Callable, List, Tuple, Dict
 class Backtester:
-  def __init__(self, strategy: Callable[[DataFrame], List[float]]):
-    self.strategy = strategy
+  def __init__(self, strategy: Callable):
 
-  def testTicker(self, ticker: str, start: str, end: str) -> List[float]: # takes in ticker symbol, start date, end date
+    pass
+
+  def testTicker(self, ticker: str, start: str, end: str, startingAmount: float = 1000000.00) -> List[float]: # takes in ticker symbol, start date, end date
     stock = yf.Ticker(ticker)
-    data = stock.h.history(start=start, end=end)
+    data = stock.history(start=start, end=end)
     data = data['Close'].to_list()
-    return self.strategy(data)
-    
-  def testTickerReport(self, ticker: str, start: str, end: str) -> Dict[str, float]: # returns a tuple of a dictionary of dates and prices, and a list of returns
+    signals = self.strategy(data)
+
+    amt = startingAmount
+    histArr = []
+    shares = 0
+    prev = 0
+    for price, signal in zip(data, signals):
+      if prev != signal:
+        if signal:
+          shares = int(amt/price)
+          amt -= shares * price
+        else:
+          amt += price * shares
+          shares = 0
+      prev = signal
+      histArr.append(shares*price+amt)
+
+      return histArr
+
+
+  def testTickerReport(self, ticker: str, start: str, end: str) -> Tuple[Dict[str, float], List[float]]: # returns a tuple of a dictionary of dates and prices, and a list of returns
     pass
 
-  def testCustom(self, data: List[float]) -> float:
-    pass
+  def testCustom(self, data: List[float], start: str, end: str, startingAmount: float = 1000000.00) -> float:
+    signals = self.strategy(data)
 
-  def testCustomReport(self, data: List[float]) -> Dict[str, float]:
+    amt = startingAmount
+    histArr = []
+    shares = 0
+    prev = 0
+    for price, signal in zip(data, signals):
+      if prev != signal:
+        if signal:
+          shares = int(amt/price)
+          amt -= shares * price
+        else:
+          amt += price * shares
+          shares = 0
+      prev = signal
+      histArr.append(shares*price+amt)
+
+      return histArr
+
+  def testCustomReport(self, data: List[float]) -> Tuple[Dict[str, float], List[float]]:
     pass
 
   def graphTicker(self, ticker: str, start: str, end: str) -> None:
